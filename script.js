546ff189756bd63cff93b2f059b75312bd36829d
BlairMcMillan <theblairmcmillan@gmail.com>
2017-02-19 10:33:03 -0600

    modal inputs and styles

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var inst = $('[data-remodal-id=modal]').remodal();
 
 
 $(window).scroll(function() {
-   	if ($(document).scrollTop() > 300 && $("#remodal").attr("displayed") === "false") {
+   	if ($(document).scrollTop() > 3000 && $("#remodal").attr("displayed") === "false") {
 		inst.open();
 		$("#remodal").attr("displayed", "true")
    	}
