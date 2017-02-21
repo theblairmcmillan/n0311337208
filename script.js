3300afa8041500588208cbabef54fdf665947814
BlairMcMillan <theblairmcmillan@gmail.com>
2017-02-21 15:34:29 -0600

    date formatting added

--- script.js
+++ script.js
@@ -25,3 +25,24 @@ $( document ).ready(function() {
 
 //fadez // 
 new WOW().init();
+
+
+// Date formatting //
+
+//Put our input DOM element into a jQuery Object
+var $jqDate = jQuery('input[name="birthday"' || 'input[name="birthday2"]');
+
+//Bind keyup/keydown to the input
+$jqDate.bind('keyup','keydown', function(e){
+  
+  //To accomdate for backspacing, we detect which key was pressed - if backspace, do nothing:
+  if(e.which !== 8) { 
+    var numChars = $jqDate.val().length;
+    if(numChars === 2 || numChars === 5){
+      var thisVal = $jqDate.val();
+      thisVal += '/';
+      $jqDate.val(thisVal);
+    }
+  }
+});
+
