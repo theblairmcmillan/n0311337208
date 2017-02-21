d9ea008565d3d90b88df86cbd68f1e226d3af68c
Emma Beaton <emma.beaton@gmail.com>
2017-02-20 20:59:55 -0600

    Fix bug with displaying modal in browser.

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
 $( document ).ready(function() {
   var is_mobile = false;
 
-  if ( $("#remodal").css('display') == 'none') {
-      is_mobile = true;
+  if ( $("#building").css('display') == 'none') {
+    is_mobile = true;
   }
 
   // now i can use is_mobile to run javascript conditionally
 
   if (is_mobile == false) {
-      //Conditional script here
-      var options = {
-        hashTracking: false
-      }
-      var inst = $('[data-remodal-id=modal]').remodal(options);
-
-      $(window).scroll(function() {
-        if ($(document).scrollTop() > 2700 && $("#remodal").attr("displayed") === "false") {
-          inst.open();
-          $("#remodal").attr("displayed", "true")
-        }
-      });
+    //Conditional script here
+    var options = {
+      hashTracking: false
     }
- });
+    var inst = $('[data-remodal-id=modal]').remodal(options);
 
+    $(window).scroll(function() {
+      if ($(document).scrollTop() > 2700 && $("#remodal").attr("displayed") === "false") {
+        inst.open();
+        $("#remodal").attr("displayed", "true")
+      }
+    });
+  }
+});
 
 //fadez // 
 new WOW().init();
